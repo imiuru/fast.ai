ecd4b5184134571a440e85e42bb83f261788462f
mayhem-am <43638852+mayhem-am@users.noreply.github.com>
2020-03-12 11:25:48 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1q6Yk4zLA1sk0vcFF1m6Z9v5_2_dXSFIj'
 export_file_name = 'export.pkl'
 
 classes = ['black', 'grizzly', 'teddys']
